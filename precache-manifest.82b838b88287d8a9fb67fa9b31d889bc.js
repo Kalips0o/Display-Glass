b6763b93f36b0e836d36b7c116323920ed157f86
Kalips0o <Kalipsoo92@gmail.com>
2022-07-28 21:30:55 +0300

    Updates

---
+++ precache-manifest.82b838b88287d8a9fb67fa9b31d889bc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e543a19f0a8999013e605d2635990d",
+    "url": "/Display-Glass/index.html"
+  },
+  {
+    "revision": "53af15e13d11a268c967",
+    "url": "/Display-Glass/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "840a762ec0030e07cb08",
+    "url": "/Display-Glass/static/css/main.e00d8547.chunk.css"
+  },
+  {
+    "revision": "53af15e13d11a268c967",
+    "url": "/Display-Glass/static/js/2.a2a82fd6.chunk.js"
+  },
+  {
+    "revision": "08ecf8de61d3e772e170ed5a37a5381e",
+    "url": "/Display-Glass/static/js/2.a2a82fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840a762ec0030e07cb08",
+    "url": "/Display-Glass/static/js/main.673da102.chunk.js"
+  },
+  {
+    "revision": "13d2277ea32818231f92",
+    "url": "/Display-Glass/static/js/runtime-main.95c169c0.js"
+  },
+  {
+    "revision": "9ae436186103c3dd01b734f3402d075f",
+    "url": "/Display-Glass/static/media/1.9ae43618.JPG"
+  },
+  {
+    "revision": "a2aab1c42ee8b6af4300905775c8c4ba",
+    "url": "/Display-Glass/static/media/1.a2aab1c4.jpg"
+  },
+  {
+    "revision": "db8241dbe61da6f27531871fc22af730",
+    "url": "/Display-Glass/static/media/1.db8241db.JPG"
+  },
+  {
+    "revision": "164efece7c1058f9b083f01c787c0d36",
+    "url": "/Display-Glass/static/media/2.164efece.jpg"
+  },
+  {
+    "revision": "7d48febe36e7fe38e017a1cc320bbee6",
+    "url": "/Display-Glass/static/media/2.7d48febe.JPG"
+  },
+  {
+    "revision": "a5b38b180b8bcabdafeaa254bafc8dc3",
+    "url": "/Display-Glass/static/media/2.a5b38b18.JPG"
+  },
+  {
+    "revision": "1d45146644d65280304d48b4ae9dff8d",
+    "url": "/Display-Glass/static/media/3.1d451466.JPG"
+  },
+  {
+    "revision": "a93e600be761f5407365243b2931f9e8",
+    "url": "/Display-Glass/static/media/3.a93e600b.jpg"
+  },
+  {
+    "revision": "da21d51e4094578f5129f9f6a9ff3284",
+    "url": "/Display-Glass/static/media/3.da21d51e.JPG"
+  },
+  {
+    "revision": "6f2855f9658479df8d03c73b30fd6091",
+    "url": "/Display-Glass/static/media/laba.6f2855f9.JPG"
+  }
+]);
