a324257d075d7b0fd49915e3ee896c096f97dae3
Kalips0o <Kalipsoo92@gmail.com>
2022-08-01 21:01:42 +0300

    deploy

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,34 +10,42 @@ export class Header extends Component {
             <>
                 <Navbar collapseOnSelect expand='lg' bg='light' font-size="20px">
                     <Container>
-                    <Navbar.Brand className={s.navBrand}>
-                    <Link to='/'onClick={() => {window.scrollTo(0, 0);}}>Display Glass
-                    </Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-                    <Navbar.Collapse id='responsive-navbar-nav'>
-                        <Nav className="ml-auto">
-                        <div className={`${s.item} ${s.active} ${s.nav}`}>
-                            <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/" onClick={() => {window.scrollTo(0, 0);}}>Главная</NavLink>
-                            <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/about" onClick={() => {window.scrollTo(0, 0);}}>О нас</NavLink>
-                            <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/product"
-                                     onClick={() => {
-                                         window.scrollTo(0, 0);
-                                     }}>Наши разработки</NavLink>
-                            <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/service"
-                                     onClick={() => {
-                                         window.scrollTo(0, 0);
-                                     }}>Услуги</NavLink>
-                            <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/contacts"
-                                     onClick={() => {
-                                         window.scrollTo(0, 0);
-                                     }}>Контакты</NavLink>
-                        </div>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-         
+                        <Navbar.Brand className={s.navBrand}>
+                            <Link to='/' onClick={() => {
+                                window.scrollTo(0, 0);
+                            }}>Display Glass
+                            </Link>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+                        <Navbar.Collapse id='responsive-navbar-nav'>
+                            <Nav className="ml-auto">
+                                <div className={`${s.item} ${s.active} ${s.nav}`}>
+                                    <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/"
+                                             onClick={() => {
+                                                 window.scrollTo(0, 0);
+                                             }}>Главная</NavLink>
+                                    <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/about"
+                                             onClick={() => {
+                                                 window.scrollTo(0, 0);
+                                             }}>О нас</NavLink>
+                                    <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/product"
+                                             onClick={() => {
+                                                 window.scrollTo(0, 0);
+                                             }}>Наши разработки</NavLink>
+                                    <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/service"
+                                             onClick={() => {
+                                                 window.scrollTo(0, 0);
+                                             }}>Услуги</NavLink>
+                                    <NavLink className={s.nav} activeClassName={s.activeLink} exact to="/contacts"
+                                             onClick={() => {
+                                                 window.scrollTo(0, 0);
+                                             }}>Контакты</NavLink>
+                                </div>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+
 
             </>
         )
