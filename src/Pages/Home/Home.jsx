21de861a2a127c9be7a66776cb41615a552810d2
Kalips0o <Kalipsoo92@gmail.com>
2022-08-25 12:10:36 +0300

    Deploy

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,6 +13,7 @@ import {Link} from 'react-router-dom';
 const Home = () => {
   return (
     <>
+      Здесь карусель
       {/*<CarouselBox />*/}
       <Container>
         <div className={s.main}>
